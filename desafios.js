0dfe0e20053fd729b80fe9c13ab47a81e4590851
Erikaloren <erikalorena43@gmail.com>
2024-03-08 18:18:17 -0500

    testing

---
+++ desafios.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//hacer un programa que salude
+/*console.log('hola')*/
+//document.write('hola')
+
+
+const nombreUsuario = prompt ('ingrese su nombre')
+switch (nombreUsuario) {
+    case (nombreUsuario):
+        console.log ("Hola" + nombreUsuario)
+        break;
+    default ("")
+    console.log ("Hola")
+    break;
+}
