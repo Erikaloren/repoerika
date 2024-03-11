bca8077cfdfa089bedae90c6d4d75edcc5ea2cc6
Erikaloren <erikalorena43@gmail.com>
2024-03-11 17:55:52 -0500

    testing again

--- desafios.js
+++ desafios.js
@@ -4,7 +4,7 @@
 /*console.log('hola')*/
 //document.write('hola')
 
-
+/*
 const nombreUsuario = prompt('ingrese su nombre')
 console.log(nombreUsuario) //siempre se pone esto?
 
@@ -13,17 +13,20 @@ switch (nombreUsuario) {
         document.write("Hola"); //será mejor con document.write?
         break;
     default:
-        document.write("Hola" + nombreUsuario);
+        document.write("Hola " + nombreUsuario);
         break;
 }
+*/
 
-
-/*const numerosUser = prompt('ingrese un número')
-console.log(numerosUser)
+const numeroUser = prompt('ingrese un número')
+console.log(numeroUser)
 
 const numeroUser1 = prompt('ingrese un número')
 console.log(numeroUser1)
 
-TERMINAR DESAFIO*/
+document.write(numeroUser + numeroUser1)
+document.write(String(numeroUser) + String(numeroUser1))
+
+//TERMINAR DESAFIO
 
 
