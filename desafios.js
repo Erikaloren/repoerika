381382cf8ef97d2b752af001c6e5c90c234b7edc
Erikaloren <erikalorena43@gmail.com>
2024-03-11 18:28:34 -0500

    d

--- desafios.js
+++ desafios.js
@@ -24,12 +24,9 @@ console.log(numeroUser)
 const numeroUser1 = prompt('ingrese un número')
 console.log(numeroUser1)
 
-numeroUser= Number(numeroUser); 
-numeroUser1= Number(numeroUser1);
+document.write(numeroUser + numeroUser1);
 
-resultado= numeroUser + numeroUser1;
 
-document.write(resultado)
 
 //TERMINAR DESAFIO*/
 
