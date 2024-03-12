5fb088e6f1e262049e1fd0dbf9af4c53c8d14602
Erikaloren <erikalorena43@gmail.com>
2024-03-11 20:44:16 -0500

    sddf

--- desafios.js
+++ desafios.js
@@ -106,10 +106,10 @@ let tipodeNumero1 = Number(prompt('ingrese un número'))
 console.log (tipodeNumero1)
 
 if (tipodeNumero < tipodeNumero1) {
-    document.write('el número mayor es ' tipodeNumero);
+    document.write('el número mayor es ' + tipodeNumero);
 }
 else (tipodeNumero1 < tipodeNumero){
-    document.write('el número mayor es ' tipodeNumero1);
+    document.write('el número mayor es ' + tipodeNumero1);
 }
 
 
