6c84b6e0923a0157efd7c2957d5e93acd551c391
Erikaloren <erikalorena43@gmail.com>
2024-04-10 22:30:45 -0500

    aprendiendo

--- desafios.js
+++ desafios.js
@@ -98,7 +98,7 @@ if (numeroSorpresa == 0){
 Hacer un programa que reciba del usuario dos números y muestre cuál es el número mayor y cuál es el número menor
 
 */
-
+/*
 
 let tipodeNumero = Number(prompt('ingrese un número'))
 console.log (tipodeNumero)
@@ -125,3 +125,61 @@ if (tipodeNumero > tipodeNumero1) {
 
 //DONE MUAHAHAHHAHHAHAHA AAAAAAHAHAHAHHAHA */
 
+/*Hacer un programa que pida un número al usuario y muestre los números pares desde 1 hasta el número ingresado 
+Cómo se muestran los números pares? D: 
+
+let Numerito = Number(prompt ('ingrese cualquier número'))
+console.log (Numerito)
+
+for (let i = 1; i <=Numerito; i++) {
+    if (i % 2 === 0) {
+        document.write(i + ", ");
+        console.log("blabla", i);
+}
+}
+
+PUDEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+
+Hacer un programa que solicite al usuario un número de 0 a 6 (manejar errores - es decir, el número no puede ser 
+diferente de 0-6). Con ese número, mostrar el día de la semana correspondiente según arreglo "días"
+
+0 - domingo
+6 - sábado (and so on)
+
+Yo creo que aquí usaría Array
+
+Hacer un programa que solicite al usuario un día de la semana (string) y le retorne el nombre del día en inglés
+
+Aquí podría asignarle un valor a cada día :thinking:
+
+*/
+/*
+
+*/
+
+
+let Numerito = Number(prompt ('ingrese cualquier número'))
+console.log (Numerito)
+
+const Days = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes'
+]
+
+if (Numerito <0 ) {
+    document.write('El número ingresado no es válido')
+} else if (Numerito >6 ) {
+    document.write('El número ingresado no es válido')
+} else {
+    console.log (Days[Numerito]);
+    document.write ('El día escogido es: ' + Days[Numerito]);
+}
+
+/*
+if (Numerito % 2) {
+    document.write("xx")
+}*/
